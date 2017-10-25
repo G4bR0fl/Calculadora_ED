541fed19b568c4a4687c5c7c21d281e3c180ce32
GabrielBessa <gabrielbessarofl@gmail.com>
2017-10-25 13:30:41 -0200

    Added infixa_to_posfixa on the main source, still unfinished

--- mainCalc.c
+++ mainCalc.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "calc.h"
+#include "char.h"
+#include <string.h>
 
 int main(){
 	
@@ -9,6 +11,7 @@ int main(){
 	printf("Informe a expressao na forma infixa: ");
 	scanf("%[^\n]s", expressao);
 	getchar();
+
 	read_expression(expressao);
 	if(read_expression(expressao) == 0){
 		printf("A expressao nao eh valida\n");
@@ -16,6 +19,8 @@ int main(){
 	if(read_expression(expressao) == 1){
 		printf("A expressao eh valida\n");
 	}
-	
+
+	infixa_to_posfixa(expressao);
+
 	return 0;
 }
