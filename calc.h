8140a7da0db2d35b461f6a3bddad6372f7617998
GabrielBessa <gabrielbessarofl@gmail.com>
2017-10-26 10:34:51 -0200

    Added menu and modo_calc functions, only menu is working at the moment

--- calc.h
+++ calc.h
@@ -5,5 +5,7 @@
 int read_expression(char* expressao);
 void infixa_to_posfixa(char* expressao);
 int prioridade(char* expressao, t_pilha* pilha, int counter);
+void modo_calc();
+int menu();
 
 #endif
