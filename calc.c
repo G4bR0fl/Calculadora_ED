cf3b1e247e70daa8c7a794548ec0eecf4021326d
GabrielBessa <gabrielbessarofl@gmail.com>
2017-10-26 10:35:55 -0200

    made some adjustments on infixa_to_posfixa, still need a lot of work to fully work, finished menu function, started modo_calc function

--- calc.c
+++ calc.c
@@ -50,23 +50,30 @@ int read_expression(char* expressao){
 int prioridade(char* expressao, t_pilha* pilha, int counter){
 	int prioridade;
 
-		if(expressao[counter] == '+' || expressao[counter] == '-'){
-			prioridade = 1;
-			return prioridade;
-		}
-		if(expressao[counter] == '*' || expressao[counter] == '/'){
-			prioridade = 2;
-			return prioridade;
-		}
+	if(expressao[counter] == '+' || expressao[counter] == '-'){
+		return 1;
+	}
+	if(expressao[counter] == '*' || expressao[counter] == '/'){
+		return 2;
+	}
 	if(expressao[counter] == '\0'){
 		return -1;
 	}
+	if(expressao[counter] == '('){
+		return 5;
+	}
+	if(expressao[counter] == '['){
+		return 4;
+	}
+	if(expressao[counter] == '{'){
+		return 3;
+	}
 }
 
-void infixa_to_posfixa(char* expressao){
+void infixa_to_posfixa(char* expressao){ /*Incompleta, ainda muita coisa a fazer e mudar o tipo pra retornar str_aux*/
 	char str_aux[101];//teste com str estatica
 	int prio_ant;
-	t_pilha* pilha = aloca_pilha();
+	t_pilha* pilha = aloca_pilha(); 
 	int i = 0, j = 0, k = 0;
 	while(expressao[k] != '\0'){
 		if(expressao[i] != '+' && expressao[i] != '-'
@@ -81,33 +88,46 @@ void infixa_to_posfixa(char* expressao){
 			j++;
 			k++;
 		}
+		//Para os operadores:
 		else{
-			if((prioridade(expressao, pilha, i) == 1 && pilha_vazia(pilha)) || prioridade(expressao, pilha, i) == 2 && pilha_vazia(pilha)){
+			if( (prioridade(expressao, pilha, i) == 1 && pilha_vazia(pilha)) || prioridade(expressao, pilha, i) == 2 && pilha_vazia(pilha) ){
 				push(pilha, expressao[i]);
 				prio_ant = prioridade(expressao, pilha, i);
 				i++;
 			}
 			else{
 				if(prioridade(expressao, pilha, i)  == 1 && !pilha_vazia(pilha)){
-					
-						str_aux[j] = pop(pilha);
+					if(prio_ant < prioridade(expressao, pilha, i)){
 						push(pilha, expressao[i]);
-						j++;
-						k++;
 						prio_ant = prioridade(expressao, pilha, i);
 						i++;
-					
+					}
+					else{
+						if(prio_ant >= prioridade(expressao, pilha, i)){
+							str_aux[j] = pop(pilha);
+							push(pilha, expressao[i]);
+							i++;
+							k++;
+							j++;
+						}
+					}
 				}
 				else{
 					if(prioridade(expressao, pilha, i) == 2 && !pilha_vazia(pilha)){
-						
-							str_aux[j] = pop(pilha);
+						if(prio_ant < prioridade(expressao, pilha, i)){
 							push(pilha, expressao[i]);
-							j++;
-							k++;
 							prio_ant = prioridade(expressao, pilha, i);
 							i++;
-						
+						}
+						else{
+							if(prio_ant >= prioridade(expressao, pilha, i)){
+								str_aux[j] = pop(pilha);
+								push(pilha, expressao[i]);
+								i++;
+								k++;
+								j++;
+							}
+						}
 					}
 					else{
 						if(prioridade(expressao, pilha, i) == -1 && !pilha_vazia(pilha)){
@@ -118,8 +138,62 @@ void infixa_to_posfixa(char* expressao){
 					}
 				}
 			}
+			//Para os parenteses, colchetes e chaves:
+			if( (prioridade(expressao, pilha, i) == 3 && pilha_vazia(pilha)) || (prioridade(expressao, pilha, i) == 4 && pilha_vazia(pilha)) || (prioridade(expressao, pilha, i) == 5 && pilha_vazia(pilha)) )
+			{
+				push(pilha, expressao[i]);
+				i++;
+				k++;
+				j++;
+				//prio_op = prioridade(expressao, pilha, i);
+			}
+			else{
+				if( (prioridade(expressao, pilha, i) == 5 && !pilha_vazia(pilha)) || (prioridade(expressao, pilha, i) == 4 && !pilha_vazia(pilha)) || (prioridade(expressao, pilha, i) == 3 && !pilha_vazia(pilha)) ){
+					push(pilha, expressao[i]);
+					i++;
+					prio_ant = 0;
+				}
+				else{
+					if(expressao[i] == ')' || expressao[i] == ']' || expressao[i] == '}'){
+						while((char)pilha->topo->dado == '*' || (char) pilha->topo->dado == '/' || (char) pilha->topo->dado == '+' || (char) pilha->topo->dado == '-'){
+							if((char) pilha->topo->dado == '(' || (char) pilha->topo->dado == '[' || (char) pilha->topo->dado == '{'){
+								pop(pilha);
+								if(!pilha_vazia(pilha)){
+									while(!pilha_vazia(pilha)){
+										str_aux[j] = pop(pilha);
+										j++;
+										k++;
+									}
+								}
+								break;
+							}
+								str_aux[j] = pop(pilha);
+								j++;
+								k++;
+						}
+					}
+				}
+			}
+
 		}
 	}
 	str_aux[k] = '\0';
 	printf("Infixa: %s\nPosfixa: %s\n", expressao, str_aux);
+}
+
+void modo_calc(){
+	t_pilha* pilha = aloca_pilha();
+	printf("Modo Calculadora\n");
+	if(pilha_vazia(pilha)){
+		printf("Pilha Vazia!");
+	}
+}
+
+int menu(){
+	int mode;
+	printf("1- Modo Expressao\n");
+	printf("2- Modo Calculadora\n");
+	printf("0- Para sair\n");
+	printf("Escolha um modo: ");
+	scanf("%d", &mode);
 }
