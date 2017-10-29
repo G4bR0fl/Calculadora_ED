e6bcd4a81eb7ca91828b244633e4d70841ff600c
GabrielBessa <gabrielbessarofl@gmail.com>
2017-10-29 16:49:16 -0200

    Added a while, to repeat the loop, until the user exits

--- mainCalc.c
+++ mainCalc.c
@@ -5,33 +5,43 @@
 #include <string.h>
 #include <math.h>
 
-int main(){
-	int mode;
+int main(){//fazer um while grande pra so sair dado um return das 3 funcoes
+	int mode, receiver;
 	mode = menu();
+	while(mode == 1 || mode == 2){
+		if(mode == 1){
+			char* expressao = (char *) malloc(sizeof(char)*100);
 
-	if(mode == 1){
-		char* expressao = (char *) malloc(sizeof(char)*100);
+			printf("Informe a expressao na forma infixa: ");
+			scanf("%[^\n]s", expressao);
+			getchar();
+			read_expression(expressao);
 
-		printf("Informe a expressao na forma infixa: ");
-		scanf("%[^\n]s", expressao);
-		getchar();
+			if(read_expression(expressao) == 0){
+				printf("A expressao nao eh valida\n");
+			}
+			if(read_expression(expressao) == 1){
+				printf("A expressao eh valida\n");
+			}
 
-		read_expression(expressao);
-		if(read_expression(expressao) == 0){
-			printf("A expressao nao eh valida\n");
+			receiver = infixa_to_posfixa(expressao);
+
+			if(receiver == 1){
+				menu();
+			}
+
 		}
-		if(read_expression(expressao) == 1){
-			printf("A expressao eh valida\n");
+		if(mode == 2){
+			modo_calc();
+			if(modo_calc() == 100){
+				menu();
+			}
+		}
+
+		if(mode == 0){
+			return 0;
 		}
 	}
-	if(mode == 2){
-		modo_calc();
-	}
-
-	if(mode == 0){
-		return 0;
-	}
-	
 
 	return 0;
 }
